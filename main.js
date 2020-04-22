f6b805cfa14f77cbc507f6184fa32ed6d219006c
Pete Stewart <petestew@gmail.com>
2020-04-22 18:21:44 -0500

    assignment cards exercise

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Dom Loops');
+const dinos = [
+    {
+        dinoType: 't-rex',
+        name: 'Tommy',
+        age: 100
+    },
+    {
+        dinoType: 'stegosaurus',
+        name: 'Steve',
+        age: 10
+    },
+    {
+        dinoType: 'raptor',
+        name: 'Ian',
+        age: 23
+    }
+];
+
+const buildDinoCards = () => {
+    // todo: build dino cards
+    const selectedDiv = document.querySelector('#dino-pen');
+    selectedDiv.innerHTML = '<h2>Dinos Go Here!<h2>';
+}
+
+const init = () => {
+    buildDinoCards();
+}
+
+init();
